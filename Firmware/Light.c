ad681eb18fba2f8a6cd36cf84d55d5dd62e56e6f
tycho <work.tycho+git@gmail.com>
2014-11-27 10:13:28 +0000

    Setup Light ADC

---
+++ Firmware/Light.c
@@ -0,0 +1,9 @@
+
+#include "Light.h"
+
+int readLight() 
+{
+  
+}
+float calibrateLight(int voltage) {}
+void outputLight(float light) {}
--- Firmware/main.c
+++ Firmware/main.c
@@ -1,5 +1,15 @@
 
-void setupADCs() {}
+#include "msp430G2553.h"
+
+#include "Light.h"
+
+#define BIT_VREF BIT4
+#define BIT_LIGHT BIT5
+void setupADCs() {
+    ADC10CTL0=SREF_1 + REFON + REFOUT + ADC10ON + ADC10SHT_3 + ADC10IE ; //1.5V ref, Ref on, Enable Ref Output, 64 clocks for sample
+    ADC10CTL1=ADC10DIV_3;
+    ADC10AE0 |= BIT_VREF + BIT_LIGHT;
+}
 
 int readTemp() {}
 float calibrateTemp(int voltage) {}
@@ -7,31 +17,27 @@ void outputTemp(float temp) {}
 
 void setHeater(float temp) {}
 
-int readLight() {}
-float calibrateLight(int voltage) {}
-void outputLight(float light) {}
-
 int readpH() {}
 float calibratepH(int voltage) {}
 void outputpH(float pH) {}
 
-int readRotation();
-int calibrateRPM(int RawRotationCount);
-void outputRPM(int RPM);
-    
-void setMotor(int RPM);
+int readRotation() {}
+int calibrateRPM(int RawRotationCount) {}
+void outputRPM(int RPM) {}
+
+void setMotor(int RPM) {}
 
 void inputLoop() {
     int tempVoltage = readTemp();
-    float tempValue = calrbrateTemp(tempVoltage);
+    float tempValue = calibrateTemp(tempVoltage);
     outputTemp(tempValue);
     
     int lightVoltage = readLight();
-    float lightValue = calbrateLight(lightVoltage);
+    float lightValue = calibrateLight(lightVoltage);
     outputLight(lightValue);
     
     int pHVoltage = readpH();
-    float pHValue = calbratepH(pHVoltage);
+    float pHValue = calibratepH(pHVoltage);
     outputpH(pHValue);
     
     int RawRotationCount = readRotation();
@@ -43,6 +49,13 @@ void inputLoop() {
 }
 
 int main() {
+  
+    WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+    
+    BCSCTL1 = CALBC1_1MHZ; // Set DCO to 1MHz
+    DCOCTL = CALDCO_1MHZ; // Set DCO to 1MHz
+    
+    
     setupADCs();
     inputLoop();
 }
